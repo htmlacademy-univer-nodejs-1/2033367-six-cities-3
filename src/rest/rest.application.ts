90bcd4de0999900559209b5a87ece346c30b67c3
Fer Andrei <andrei.fer@urfu.me>
2024-04-02 15:51:06 +0500

    3.13 Added: Config to REST Application

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,10 +1,14 @@
+import type { Config } from '../shared/libs/config/config.interface.js';
 import type { Logger } from '../shared/logger/index.js';
 
 export class RestApplication {
-  constructor(private readonly logger: Logger) {}
+  constructor(
+    private readonly logger: Logger,
+    private readonly config: Config
+  ) {}
 
   public async init() {
     this.logger.info('Application initialization');
-    this.logger.debug('This is debug');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
