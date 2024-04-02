c9f2cce5160bec19bca1971d582cdfa51e663c5b
Fer Andrei <andrei.fer@urfu.me>
2024-04-02 14:20:50 +0500

    3.3 Added: Logger implementation

---
+++ src/shared/logger/pino.logger.ts
@@ -0,0 +1,28 @@
+import { pino, type Logger as PinoInstance } from 'pino';
+import type { Logger } from './logger.interface';
+
+
+export class PinoLogger implements Logger {
+  private readonly logger: PinoInstance;
+
+  constructor() {
+    this.logger = pino();
+  }
+
+  public debug(message: string, ...args: unknown[]): void {
+    this.logger.debug(message, ...args);
+  }
+
+  public info(message: string, ...args: unknown[]): void {
+    this.logger.info(message, ...args);
+  }
+
+  public warn(message: string, ...args: unknown[]): void {
+    this.logger.warn(message, ...args);
+  }
+
+  public error(message: string, error: Error, ...args: unknown[]): void {
+    this.logger.error(error, message, ...args);
+  }
+
+}
