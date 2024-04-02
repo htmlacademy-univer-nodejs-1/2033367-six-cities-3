d4f03a63ee9b1778b5ee840a8c86e0c447b50ad9
Fer Andrei <andrei.fer@urfu.me>
2024-04-02 16:30:22 +0500

    3.17 Update: RestConfig class

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,10 +1,11 @@
 import type { Config } from '../shared/libs/config/config.interface.js';
+import type { RestSchema } from '../shared/libs/config/rest.schema.js';
 import type { Logger } from '../shared/logger/index.js';
 
 export class RestApplication {
   constructor(
     private readonly logger: Logger,
-    private readonly config: Config<>
+    private readonly config: Config<RestSchema>
   ) {}
 
   public async init() {
--- src/shared/libs/config/rest.config.ts
+++ src/shared/libs/config/rest.config.ts
@@ -1,9 +1,10 @@
-import { type DotenvParseOutput, config } from 'dotenv';
+import { config } from 'dotenv';
 import type { Logger } from '../../logger';
 import type { Config } from './config.interface';
+import { configRestSchema, type RestSchema } from './rest.schema';
 
-export class RestConfig implements Config {
-  private readonly config: NodeJS.ProcessEnv;
+export class RestConfig implements Config<RestSchema> {
+  private readonly config: RestSchema;
 
   constructor(private readonly logger: Logger) {
     const parsedOutput = config();
@@ -12,11 +13,19 @@ export class RestConfig implements Config {
       throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
     }
 
-    this.config = <DotenvParseOutput>parsedOutput.parsed;
+    configRestSchema.load({});
+
+    try {
+      configRestSchema.validate({ allowed: 'strict', output: this.logger.info });
+    } catch(error) {
+      this.logger.error('Schema does not pass validation', error as Error);
+    }
+
+    this.config = configRestSchema.getProperties();
     this.logger.info('.env file found and successfully parsed!');
   }
 
-  public get(key: string): string | undefined {
+  public get<T extends keyof RestSchema>(key: T): RestSchema[T] {
     return this.config[key];
   }
 }
