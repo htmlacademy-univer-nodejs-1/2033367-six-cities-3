324fdf50daeffcc24f337d980bc890c4f64cf2db
Fer Andrei <andrei.fer@urfu.me>
2024-04-02 17:10:18 +0500

    3.24 Added: Injection in RestApplication

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,11 +1,14 @@
+import { inject, injectable } from 'inversify';
 import type { Config } from '../shared/libs/config/config.interface.js';
 import type { RestSchema } from '../shared/libs/config/rest.schema.js';
 import type { Logger } from '../shared/logger/index.js';
+import { Component } from '../shared/types/component.enum.js';
 
+@injectable()
 export class RestApplication {
   constructor(
-    private readonly logger: Logger,
-    private readonly config: Config<RestSchema>
+    @inject(Component.Logger) private readonly logger: Logger,
+    @inject(Component.Config) private readonly config: Config<RestSchema>
   ) {}
 
   public async init() {
