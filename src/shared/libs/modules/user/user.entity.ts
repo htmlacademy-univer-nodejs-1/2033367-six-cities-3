1cd65c8b4ef29ae970d2f7471446c07046f2b7fa
Fer Andrei <andrei.fer@urfu.me>
2024-04-13 19:02:03 +0500

    4.18 Added: createdAt & updatedAt fields

--- src/shared/libs/modules/user/user.entity.ts
+++ src/shared/libs/modules/user/user.entity.ts
@@ -1,7 +1,7 @@
-import { getModelForClass, prop } from '@typegoose/typegoose';
+import { defaultClasses, getModelForClass, prop } from '@typegoose/typegoose';
 import type { User, UserType } from '../../../types/index.js';
 
-export class UserEntity implements User {
+export class UserEntity extends defaultClasses.TimeStamps implements User {
   @prop({ required: true, minlength: [1, 'Min length for name is 1'], maxlength: [15, 'Max length for name is 15'] })
   public name = '';
 
