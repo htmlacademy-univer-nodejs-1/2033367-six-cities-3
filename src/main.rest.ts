60dda23dd2d0275deaff8a56f0f2fabe1ab0d858
Fer Andrei <andrei.fer@urfu.me>
2024-04-13 16:54:34 +0500

    4.8 Added: MongoDBClient binding

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -4,12 +4,15 @@ import { RestApplication } from './rest/index.js';
 import { PinoLogger, type Logger } from './shared/logger/index.js';
 import { Component } from './shared/types/index.js';
 import { type Config, RestConfig, type RestSchema } from './shared/libs/config/index.js';
+import type { DatabaseClient } from './shared/libs/database-client/database-client.interface.js';
+import { MongoDatabaseClient } from './shared/libs/database-client/mongo.database-client.js';
 
 async function bootstrap() {
   const container = new Container();
   container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
   container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
   container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
+  container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
 
   const application = container.get<RestApplication>(Component.RestApplication);
   await application.init();
