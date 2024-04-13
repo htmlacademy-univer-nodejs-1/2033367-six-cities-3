0c68afa88addf305b0a2d2555affcdcc685dc5f6
Fer Andrei <andrei.fer@urfu.me>
2024-04-13 17:40:00 +0500

    4.9 Added: Connection to Mongo & Fix ports

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -3,16 +3,35 @@ import type { Config } from '../shared/libs/config/config.interface.js';
 import type { RestSchema } from '../shared/libs/config/rest.schema.js';
 import type { Logger } from '../shared/logger/index.js';
 import { Component } from '../shared/types/component.enum.js';
+import type { DatabaseClient } from '../shared/libs/database-client/database-client.interface.js';
+import { getMongoURI } from '../shared/helpers/database.js';
 
 @injectable()
 export class RestApplication {
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
-    @inject(Component.Config) private readonly config: Config<RestSchema>
+    @inject(Component.Config) private readonly config: Config<RestSchema>,
+    @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
   ) {}
+
+  private async _initDb() {
+    const mongoUri = getMongoURI(
+      this.config.get('DB_USER'),
+      this.config.get('DB_PASSWORD'),
+      this.config.get('DB_HOST'),
+      this.config.get('DB_PORT'),
+      this.config.get('DB_NAME'),
+    );
+
+    return this.databaseClient.connect(mongoUri);
+  }
 
   public async init() {
     this.logger.info('Application initialization');
     this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
+
+    this.logger.info('Initializing database');
+    await this._initDb();
+    this.logger.info('Initialization of database completed');
   }
 }
