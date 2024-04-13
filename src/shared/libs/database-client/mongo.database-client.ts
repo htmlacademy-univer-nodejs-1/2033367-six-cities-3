866a0040343eebcfcf66004708cf143e38da114f
Fer Andrei <andrei.fer@urfu.me>
2024-04-13 16:46:19 +0500

    4.5 Added: MongoDB client

---
+++ src/shared/libs/database-client/mongo.database-client.ts
@@ -0,0 +1,45 @@
+import { inject, injectable } from 'inversify';
+import type { DatabaseClient } from './database-client.interface.js';
+import * as Mongoose from 'mongoose';
+import { Component } from '../../types/index.js';
+import type { Logger } from '../../logger/index.js';
+
+@injectable()
+export class MongoDatabaseClient implements DatabaseClient {
+  private mongoose: typeof Mongoose;
+  private isConnected: boolean;
+
+  constructor(
+    @inject(Component.Logger) private readonly logger: Logger
+  ) {
+    this.isConnected = false;
+  }
+
+  public isConnectedToDatabase() {
+    return this.isConnected;
+  }
+
+  public async connect(uri: string): Promise<void> {
+    if (this.isConnectedToDatabase()) {
+      throw new Error('MongoDB client already connected');
+    }
+
+    this.logger.info('Trying to connect to MongoDB');
+
+    this.mongoose = await Mongoose.connect(uri);
+    this.isConnected = true;
+
+    this.logger.info('Database connection established');
+  }
+
+  public async disconnect(): Promise<void> {
+    if (!this.isConnectedToDatabase()) {
+      throw new Error('Not connected to database');
+    }
+
+    await this.mongoose.disconnect?.();
+    this.isConnected = false;
+    this.logger.info('Database connection closed');
+  }
+
+}
