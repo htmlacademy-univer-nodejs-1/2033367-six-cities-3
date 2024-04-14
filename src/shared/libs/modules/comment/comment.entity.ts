4d502b02bcc0e5724ade2cb9db18643dd4b48bda
Fer Andrei <andrei.fer@urfu.me>
2024-04-14 14:08:08 +0500

    5.11 Added: Comment entity

---
+++ src/shared/libs/modules/comment/comment.entity.ts
@@ -0,0 +1,22 @@
+import { defaultClasses, getModelForClass, prop, type Ref } from '@typegoose/typegoose';
+import { OfferEntity } from '../offer';
+import { UserEntity } from '../user';
+
+export class CommentEntity extends defaultClasses.TimeStamps {
+  @prop({ trim: true, required: true })
+  public text: string;
+
+  @prop({
+    ref: OfferEntity,
+    required: true,
+  })
+  public offerId: Ref<OfferEntity>;
+
+  @prop({
+    ref: UserEntity,
+    required: true,
+  })
+  public userId: Ref<UserEntity>;
+}
+
+export const CommentModel = getModelForClass(CommentEntity);
