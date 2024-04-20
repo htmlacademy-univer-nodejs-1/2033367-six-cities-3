39bcefd9457c32ebbe85ff7a6cb16a4b4bb2556f
Fer Andrei <andrei.fer@urfu.me>
2024-04-20 16:27:21 +0500

    6.4 Added: Controller interface

---
+++ src/shared/libs/rest/controller/controller.interface.ts
@@ -0,0 +1,11 @@
+import type { Router } from 'express';
+import type { Route } from '../types/route.interface';
+
+export interface Controller {
+  readonly router: Router;
+  addRoute(route: Route): void;
+  send<T>(res: Response, statusCode: number, data: T): void;
+  ok<T>(res: Response, data: T): void;
+  created<T>(res: Response, data: T): void;
+  noContent<T>(res: Response, data: T): void;
+}
--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -1,2 +1,3 @@
 export * from './types/http-method.enum.js';
 export * from './types/route.interface.js';
+export * from './controller/controller.interface.js';
