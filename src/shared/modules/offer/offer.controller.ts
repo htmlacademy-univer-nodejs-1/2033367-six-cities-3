b38d37bc9dfb43c330c83bb298fd42996b5f0549
Fer Andrei <andrei.fer@urfu.me>
2024-04-20 18:17:05 +0500

    6.9 Added: Offer controller

---
+++ src/shared/modules/offer/offer.controller.ts
@@ -0,0 +1,26 @@
+import type { Request, Response } from 'express';
+import { inject } from 'inversify';
+import { BaseController } from '../../libs/rest/controller/base-controller.abstract.js';
+import { Component } from '../../types/index.js';
+import type { Logger } from '../../libs/logger/index.js';
+import { HttpMethod } from '../../libs/rest/index.js';
+import type { OfferService } from './offer-service.interface.js';
+
+export class OfferController extends BaseController {
+
+  constructor(
+    @inject(Component.Logger) protected readonly logger: Logger,
+    @inject(Component.OfferService) private readonly offerService: OfferService,
+  ) {
+    super(logger);
+
+    this.logger.info('Register router for OfferController');
+    this.addRoute({ path: 'offers', method: HttpMethod.Get, handler: this.index });
+  }
+
+  public async index(_req: Request, res: Response): Promise<void> {
+    const offers = await this.offerService.find();
+    this.ok(res, offers);
+  }
+
+}
