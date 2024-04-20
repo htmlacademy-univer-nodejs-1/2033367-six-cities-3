49d04c3133c44f2fdb8c4573b8ed5ffb630fda47
Fer Andrei <andrei.fer@urfu.me>
2024-04-20 19:44:41 +0500

    6.22 Added: HttpErrors to DefaultExceptionFilter

--- src/shared/libs/rest/exception-filter/default-exception-filter.ts
+++ src/shared/libs/rest/exception-filter/default-exception-filter.ts
@@ -4,6 +4,8 @@ import { inject, injectable } from 'inversify';
 import { Component } from '../../../types/index.js';
 import type { Logger } from '../../logger/index.js';
 import { StatusCodes } from 'http-status-codes';
+import { HttpError } from '../errors/http-error.js';
+import { createErrorObject } from '../../../helpers/common.js';
 
 @injectable()
 export class DefaultExceptionFilter implements ExceptionFilter {
@@ -14,10 +16,25 @@ export class DefaultExceptionFilter implements ExceptionFilter {
     this.logger.info('Register DefaultExceptionFilter');
   }
 
-  public catch(error: Error, _req: Request, res: Response, _next: NextFunction): void {
+  private handleHttpError(error: HttpError, _req: Request, res: Response, _next: NextFunction) {
+    this.logger.error(`[${error.detail}]: ${error.httpStatusCode} — ${error.message}`, error);
+    res
+      .status(error.httpStatusCode)
+      .json(createErrorObject(error.message));
+  }
+
+  private handleOtherError(error: Error, _req: Request, res: Response, _next: NextFunction) {
     this.logger.error(error.message, error);
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ error: error.message });
+      .json(createErrorObject(error.message));
+  }
+
+  public catch(error: Error | HttpError, req: Request, res: Response, next: NextFunction): void {
+    if (error instanceof HttpError) {
+      return this.handleHttpError(error, req, res, next);
+    }
+
+    this.handleOtherError(error, req, res, next);
   }
 }
