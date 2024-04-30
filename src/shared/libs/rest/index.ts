5d43dd9722ad9f9fe78bb1ae8494d5a19085b9c7
Fer Andrei <andrei.fer@urfu.me>
2024-04-30 11:30:05 +0500

    7.7 Added: ValidateObjectIDMiddleware

--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -5,4 +5,4 @@ export * from './types/request-body.type.js';
 export * from './types/request-params.type.js';
 export * from './exception-filter/exception-filter.interface.js';
 export * from './errors/index.js';
-export * from './middleware/middleware.interface.js';
+export * from './middleware/index.js';
