3c38154e3c29d412840b209b4e92d2916275be3a
Fer Andrei <andrei.fer@urfu.me>
2024-04-30 12:51:18 +0500

    7.10 Added: Validators for CreateOfferDTO

--- src/shared/modules/offer/dto/create-offer.dto.ts
+++ src/shared/modules/offer/dto/create-offer.dto.ts
@@ -1,22 +1,67 @@
 import type { Ref } from '@typegoose/typegoose';
-import type { City, Coordinates, Facility, HousingType } from '../../../../types/index.js';
+import { City, Facility, HousingType, type Coordinates } from '../../../../shared/types/index.js';
 import type { UserEntity } from '../../user/user.entity.js';
+import { IsArray, IsBoolean, IsDateString, IsEnum, IsInt, IsMongoId, IsObject, MaxLength, MinLength } from 'class-validator';
+import { CreateOfferValidationMessage } from './create-offer.messages.js';
 
 export class CreateOfferDTO {
+
+  @MinLength(10, { message: CreateOfferValidationMessage.title.minLength })
+  @MaxLength(100, { message: CreateOfferValidationMessage.title.maxLength })
   public title: string;
+
+  @MinLength(20)
+  @MaxLength(1024)
   public description: string;
+
+  @IsDateString({}, { message: CreateOfferValidationMessage.postDate.invalidFormat })
   public postDate: Date;
+
+  @IsEnum(City, { message: CreateOfferValidationMessage.city.invalid })
   public city: City;
+
+  @MaxLength(256, { message: CreateOfferValidationMessage.preview.maxLength })
   public preview: string;
+
+  @IsArray({ each: true, message: CreateOfferValidationMessage.picutres.invalidFormat })
   public pictures: string[];
+
+  @IsBoolean({ message: CreateOfferValidationMessage.isPremium.invalidFormat })
   public isPremium: boolean;
+
+  @IsBoolean({ message: CreateOfferValidationMessage.isFavorite.invalidFormat })
   public isFavorite: boolean;
+
+  @IsInt({ message: CreateOfferValidationMessage.rating.invalidFormat })
+  @MinLength(1, { message: CreateOfferValidationMessage.rating.minValue })
+  @MaxLength(5, { message: CreateOfferValidationMessage.rating.maxValue })
   public rating: number;
+
+  @IsEnum(HousingType, { message: CreateOfferValidationMessage.housingType.invalid })
   public housingType: HousingType;
+
+  @IsInt({ message: CreateOfferValidationMessage.roomsCount.invalidFormat })
+  @MinLength(1, { message: CreateOfferValidationMessage.roomsCount.minValue })
+  @MaxLength(8, { message: CreateOfferValidationMessage.roomsCount.maxValue })
   public roomsCount: number;
+
+  @IsInt({ message: CreateOfferValidationMessage.guestsCount.invalidFormat })
+  @MinLength(1, { message: CreateOfferValidationMessage.guestsCount.minValue })
+  @MaxLength(10, { message: CreateOfferValidationMessage.guestsCount.maxValue })
   public guestsCount: number;
+
+  @IsInt({ message: CreateOfferValidationMessage.price.invalidFormat })
+  @MinLength(100, { message: CreateOfferValidationMessage.price.minValue })
+  @MaxLength(100000, { message: CreateOfferValidationMessage.price.maxValue })
   public price: number;
+
+  @IsArray({ message: CreateOfferValidationMessage.facilities.invalidFormat })
+  @IsEnum(Facility, { each: true, message: CreateOfferValidationMessage.facilities.invalid })
   public facilities: Facility[];
-  public authorId: Ref<UserEntity>;
+
+  @IsMongoId({ message: CreateOfferValidationMessage.author.invalidId })
+  public authorId: string;
+
+  @IsObject({ message: CreateOfferValidationMessage.coordinates.invalid })
   public coordinates: Coordinates;
 }
---
+++ src/shared/modules/offer/dto/create-offer.messages.ts
@@ -0,0 +1,62 @@
+export const CreateOfferValidationMessage = {
+  title: {
+    minLength: 'Minimum title length must be 10',
+    maxLength: 'Maximum title length must be 100',
+  },
+  description: {
+    minLength: 'Minimum description length must be 20',
+    maxLength: 'Maximum description length must be 1024',
+  },
+  postDate: {
+    invalidFormat: 'postDate must be a valid ISO date'
+  },
+  city: {
+    invalid: 'City must be one of Paris, Cologne, Brussels, Amsterdam, Hamburg and Dusseldorf'
+  },
+  preview: {
+    maxLength: 'Too long for field «preview»',
+  },
+  picutres: {
+    invalidFormat: 'Field picutres must be an array',
+    invalid: 'Pictures must be an array of valid pictures'
+  },
+  isPremium: {
+    invalidFormat: 'Field isPremium must be a boolean',
+  },
+  isFavorite: {
+    invalidFormat: 'Field isFavorite must be a boolean',
+  },
+  rating: {
+    invalidFormat: 'rating must be an integer',
+    minValue: 'Minimum rating is 1',
+    maxValue: 'Maximum rating is 5'
+  },
+  housingType: {
+    invalid: 'Field housingType must be one of apartment, house, room and hotel'
+  },
+  roomsCount: {
+    invalidFormat: 'roomsCount must be an integer',
+    minValue: 'Minimum roomsCount is 1',
+    maxValue: 'Maximum roomsCount is 8'
+  },
+  guestsCount: {
+    invalidFormat: 'guestsCount must be an integer',
+    minValue: 'Minimum guestsCount is 1',
+    maxValue: 'Maximum guestsCount is 10'
+  },
+  price: {
+    invalidFormat: 'Price must be an integer',
+    minValue: 'Minimum price is 100',
+    maxValue: 'Maximum price is 100000'
+  },
+  facilities: {
+    invalidFormat: 'Field facilities must be an array',
+    invalid: 'Facilities must be an array of valid facilities'
+  },
+  author: {
+    invalidId: 'author field must be a valid id'
+  },
+  coordinates: {
+    invalid: 'City must be one of Paris, Cologne, Brussels, Amsterdam, Hamburg and Dusseldorf'
+  },
+} as const;
